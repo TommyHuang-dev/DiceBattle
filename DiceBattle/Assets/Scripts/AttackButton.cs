2620fd07821b35dd846bc847436225b7ab02cfb5
Tommy Huang <tommyhuang999@gmail.com>
2022-07-16 20:59:14 -0400

    add button that rolls stuff

--- DiceBattle/Assets/Scripts/AttackButton.cs
+++ DiceBattle/Assets/Scripts/AttackButton.cs
@@ -1,19 +1,61 @@
 using UnityEngine;
 using UnityEngine.UI;
-using System.Collections;
+using System.Collections.Generic;
 
 public class AttackButton : MonoBehaviour
 {
-	public Button yourButton;
+	public Button button;
+	// the base attack / dice roll
+	// key: dx  value: number of dice
+	private Dictionary<int, int> dice;
 
 	void Start()
 	{
-		Button btn = yourButton.GetComponent<Button>();
+		Button btn = button.GetComponent<Button>();
 		btn.onClick.AddListener(TaskOnClick);
+		dice = new Dictionary<int, int>
+		{
+			{4, 2 },
+            {8, 1 }
+		};
+	}
+
+	List<int> RollNums()
+    {
+		List<int> rolls = new List<int>();
+		foreach (var die in dice)
+		{
+			for (int i = 0; i < die.Value; i++)
+            {
+				rolls.Add(Random.Range(1, die.Key + 1));
+            }
+		}
+		return rolls;
 	}
 
 	void TaskOnClick()
-	{
-		Debug.Log("You have clicked the button!");
+	{ 
+		Debug.Log(GetText());
+		List<int> rolls = RollNums();
+		string rollstring = "You have rolled:";
+		foreach (var roll in rolls)
+        {
+			rollstring = rollstring + " " + roll;
+        }
+		Debug.Log(rollstring);
+	}
+
+	public string GetText()
+    {
+        string result = "";
+        foreach (var die in dice)
+		{
+			if (result != "")
+            {
+				result += ", ";
+            }
+			result = result + die.Value + "d" + die.Key;
+		}
+		return result;
 	}
 }
